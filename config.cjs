a7a530d9e4b944ecb22ff1daa8bf842535008019
HashKim025 <143814922+HashKim025@users.noreply.github.com>
2024-09-29 05:47:48 +0300

    Update config.cjs

--- config.cjs
+++ config.cjs
@@ -6,6 +6,7 @@ const config = {
   SESSION_ID: process.env.SESSION_ID || "Rushmd~0cXsyAi6",
   PREFIX: process.env.PREFIX || '.',
   MODE: process.env.MODE || "public",
+  AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true"
   };
 
 
